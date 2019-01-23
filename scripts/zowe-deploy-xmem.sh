e30d006e133c4cfccf6e5c9d4fbbaa54564e5033
Irek Fakhrutdinov <ifakhrutdinov@rocketsoftware.com>
2019-01-23 13:05:20 +0100

    Better reporting

--- scripts/zowe-deploy-xmem.sh
+++ scripts/zowe-deploy-xmem.sh
@@ -44,37 +44,63 @@ sed -i "s/${XMEM_ELEMENT_ID}.S${XMEM_ELEMENT_ID}LOAD/${XMEM_LOADLIB}/g" ${ZSS}/S
 sed -i "s/${XMEM_ELEMENT_ID}.S${XMEM_ELEMENT_ID}PARM/${XMEM_PARMLIB}/g" ${ZSS}/SAMPLIB/${XMEM_JCL}.tmp
 
 # 1. Deploy loadlib
-sh $BASEDIR/zowe-xmem-deploy-loadmodule.sh ${ZSS} ${XMEM_LOADLIB} ${XMEM_MODULE}
+echo
+echo "************************ Install step 'LOADLIB' start **************************"
+loadlibCmd1="sh $BASEDIR/zowe-xmem-deploy-loadmodule.sh ${ZSS} ${XMEM_LOADLIB} ${XMEM_MODULE}"
+$loadlibCmd1
 if [[ $? -eq 0 ]]
 then
   # 2. APF-authorize loadlib
+  echo
+  echo "************************ Install step 'APF-auth' start *************************"
   loadlibOk=true
-  sh $BASEDIR/zowe-xmem-apf.sh ${XMEM_LOADLIB}
+  apfCmd1="sh $BASEDIR/zowe-xmem-apf.sh ${XMEM_LOADLIB}"
+  $apfCmd1
   if [[ $? -eq 0 ]]; then
     apfOk=true
   fi
+  echo "************************ Install step 'APF-auth' end ***************************"
 fi
+echo "************************ Install step 'LOADLIB' end ****************************"
+
 
 # 3. Deploy parmlib
-sh $BASEDIR/zowe-xmem-deploy-parmlib.sh ${ZSS} ${XMEM_PARMLIB} ${XMEM_PARM}
+echo
+echo "************************ Install step 'PARMLIB' start **************************"
+parmlibCmd1="sh $BASEDIR/zowe-xmem-deploy-parmlib.sh ${ZSS} ${XMEM_PARMLIB} ${XMEM_PARM}"
+$parmlibCmd1
 if [[ $? -eq 0 ]]
   parmlibOk=true
 then
 fi
+echo "************************ Install step 'PARMLIB' end ****************************"
+
 
 # 4. Deploy PROCLIB
-sh $BASEDIR/zowe-xmem-deploy-proclib.sh ${ZSS} ${XMEM_PROCLIB} ${XMEM_JCL}
+echo
+echo "************************ Install step 'PROCLIB' start **************************"
+proclibCmd1="sh $BASEDIR/zowe-xmem-deploy-proclib.sh ${ZSS} ${XMEM_PROCLIB} ${XMEM_JCL}"
+$proclibCmd1
 if [[ $? -eq 0 ]]
   proclibOk=true
 then
 fi
+echo "************************ Install step 'PROCLIB' end ****************************"
+
 
 # 5. PPT-entry
-sh $BASEDIR/zowe-xmem-ppt.sh ${XMEM_MODULE} ${XMEM_KEY}
+echo
+echo "************************ Install step 'PPT-entry' start ************************"
+pptCmd1="sh $BASEDIR/zowe-xmem-ppt.sh ${XMEM_MODULE} ${XMEM_KEY}"
+$pptCmd1
 if [[ $? -eq 0 ]]
   pptOk=true
 then
 fi
+echo "************************ Install step 'PPT-entry' end **************************"
+
+
+
 
 # Security install steps
 
@@ -89,7 +115,10 @@ else
 fi
 }
 
+
 # 6. Get SAF
+echo
+echo "************************ Install step 'SAF-type' start *************************"
 echo "Get SAF"
 for saf in RACF ACF2 TSS
 do
@@ -97,56 +126,81 @@ do
     echo "Info:  SAF=${saf}"
     safOk=true
     break
-  else
-    echo "Error:  SAF has not been found"
   fi
 done
+echo "************************ Install step 'SAF-type' end ***************************"
 
-if [[ $safOk -eq true ]]
-then
+if ! $safOk ; then
+  echo "Error:  SAF has not been found"
+fi
+
+if $safOk ; then
+
 
   # 7. Handle STC user
-  sh $BASEDIR/zowe-xmem-check-user.sh ${saf} ${XMEM_STC_USER}
+  echo
+  echo "************************ Install step 'STC user' start *************************"
+  stcUserCmd1="sh $BASEDIR/zowe-xmem-check-user.sh ${saf} ${XMEM_STC_USER}"
+  $stcUserCmd1
   rc=$?
   if [[ $rc -eq 1 ]]; then
-    sh $BASEDIR/zowe-xmem-define-stc-user.sh ${saf} ${XMEM_STC_USER} ${XMEM_STC_USER_UID}
+    stcUserCmd2="sh $BASEDIR/zowe-xmem-define-stc-user.sh ${saf} ${XMEM_STC_USER} ${XMEM_STC_USER_UID}"
+    $stcUserCmd2
     if [[ $? -eq 0 ]]; then
       stcUserOk=true
     fi
   elif [[ $rc -eq 0 ]]; then
     stcUserOk=true
   fi
+  echo "************************ Install step 'STC user' end ***************************"
+
 
   # 8. Handle STC profile
-  sh $BASEDIR/zowe-xmem-check-stc-profile.sh ${saf} ${XMEM_STC_PREFIX}
+  echo
+  echo "************************ Install step 'STC profile' start **********************"
+  stcProfileCmd1="sh $BASEDIR/zowe-xmem-check-stc-profile.sh ${saf} ${XMEM_STC_PREFIX}"
+  $stcProfileCmd1
   rc=$?
   if [[ $rc -eq 1 ]]; then
-    sh $BASEDIR/zowe-xmem-define-stc-profile.sh ${saf} ${XMEM_STC_PREFIX} ${XMEM_STC_USER}
+    stcProfileCmd2="sh $BASEDIR/zowe-xmem-define-stc-profile.sh ${saf} ${XMEM_STC_PREFIX} ${XMEM_STC_USER}"
+    $stcProfileCmd2
     if [[ $? -eq 0 ]]; then
       stcProfileOk=true
     fi
   elif [[ $rc -eq 0 ]]; then
     stcProfileOk=true
   fi
+  echo "************************ Install step 'STC profile' end ************************"
+
 
   # 9. Handle security profile
-  sh $BASEDIR/zowe-xmem-check-profile.sh ${saf} FACILITY ${XMEM_PROFILE} ${ZOWE_USER}
+  echo
+  echo "************************ Install step 'Security profile' start *****************"
+  xmemProfileCmd1="sh $BASEDIR/zowe-xmem-check-profile.sh ${saf} FACILITY ${XMEM_PROFILE} ${ZOWE_USER}"
+  $xmemProfileCmd1
   rc=$?
   if [[ $rc -eq 1 ]]; then
-    sh $BASEDIR/zowe-xmem-define-xmem-profile.sh ${saf} ${XMEM_PROFILE}
+    xmemProfileCmd2="sh $BASEDIR/zowe-xmem-define-xmem-profile.sh ${saf} ${XMEM_PROFILE}"
+    $xmemProfileCmd2
     if [[ $? -eq 0 ]]; then
       xmemProfileOk=true
     fi
   elif [[ $rc -eq 0 ]]; then
     xmemProfileOk=true
   fi
+  echo "************************ Install step 'Security profile' end *******************"
+
 
   # 10. Check access
+  echo
+  echo "************************ Install step 'Security profile access' start **********"
   if [[ "$xmemProfileOk" = "true" ]]; then
-    sh $BASEDIR/zowe-xmem-check-access.sh ${saf} FACILITY ${XMEM_PROFILE} ${ZOWE_USER}
+    xmemAccessCmd1="sh $BASEDIR/zowe-xmem-check-access.sh ${saf} FACILITY ${XMEM_PROFILE} ${ZOWE_USER}"
+    $xmemAccessCmd1
     rc=$?
     if [[ $rc -eq 1 ]]; then
-      sh $BASEDIR/zowe-xmem-permit.sh ${saf} ${XMEM_PROFILE} ${ZOWE_USER}
+      xmemAccessCmd2="sh $BASEDIR/zowe-xmem-permit.sh ${saf} ${XMEM_PROFILE} ${ZOWE_USER}"
+      $xmemAccessCmd2
       if [[ $? -eq 0 ]]; then
         xmemProfileAccessOk=true
       fi
@@ -154,41 +208,54 @@ then
       xmemProfileAccessOk=true
     fi
   fi
+  echo "************************ Install step 'Security profile access' end ************"
+
 
 fi
 
-echo "****************************************"
-echo "**************** Report ****************"
-echo "****************************************"
+echo
+echo "********************************************************************************"
+echo "************************************ Report ************************************"
+echo "********************************************************************************"
 
 if $loadlibOk ; then
   echo "LOADLIB - Ok"
 else
   echo "LOADLIB - Error"
+  echo "Please correct errors and re-run the following scripts:"
+  echo $loadlibCmd1
 fi
 
 if $apfOk ; then
   echo "APF-auth - Ok"
 else
   echo "APF-auth - Error"
+  echo "Please correct errors and re-run the following scripts:"
+  echo $apfCmd1
 fi
 
 if $parmlibOk ; then
   echo "PARMLIB - Ok"
 else
   echo "PARMLIB - Error"
+  echo "Please correct errors and re-run the following scripts:"
+  echo $parmlibCmd1
 fi
 
 if $proclibOk ; then
   echo "PROCLIB - Ok"
 else
   echo "PROCLIB - Error"
+  echo "Please correct errors and re-run the following scripts:"
+  echo $proclibCmd1
 fi
 
 if $pptOk ; then
   echo "PPT-entry - Ok"
 else
   echo "PPT-entry - Error"
+  echo "Please correct errors and re-run the following scripts:"
+  echo $pptCmd1
 fi
 
 if $safOk ; then
@@ -199,29 +266,49 @@ fi
 
 if $stcUserOk ; then
   echo "STC user - Ok"
+elif ! $safOk ; then
+  echo "STC user - N/A"
 else
   echo "STC user - Error"
+  echo "Please correct errors and re-run the following scripts:"
+  echo $stcUserCmd1
+  echo $stcUserCmd2
 fi
 
 if $stcProfileOk ; then
   echo "STC profile - Ok"
+elif ! $safOk ; then
+  echo "STC profile - N/A"
 else
   echo "STC profile - Error"
+  echo "Please correct errors and re-run the following scripts:"
+  echo $stcProfileCmd1
+  echo $stcProfileCmd2
 fi
 
 if $xmemProfileOk ; then
   echo "Security profile - Ok"
+elif ! $safOk ; then
+  echo "Security profile - N/A"
 else
   echo "Securoty profile - Error"
+  echo "Please correct errors and re-run the following scripts:"
+  echo $xmemProfileCmd1
+  echo $xmemProfileCmd2
 fi
 
 if $xmemProfileAccessOk ; then
   echo "Security profile access - Ok"
+elif ! $safOk ; then
+  echo "Security profile access - N/A"
 else
   echo "Security profile access - Error"
+  echo "Please correct errors and re-run the following scripts:"
+  echo $xmemAccessCmd1
+  echo $xmemAccessCmd2
 fi
 
-echo "****************************************"
-echo "****************************************"
-echo "****************************************"
+echo "********************************************************************************"
+echo "********************************************************************************"
+echo "********************************************************************************"
 
